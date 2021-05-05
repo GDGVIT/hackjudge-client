d54c0b8daa7e1279a6728285ce9d19140fe8f506
Pranjal Timsina <pranjal.timsina@live.com>
2021-05-05 14:46:48 +0545

    feat: create manage submission and manage team component

---
+++ src/components/participant-components/ManageSubmission.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManageSubmission = () => {
+  return (
+    <div>
+      This is the submissions page
+    </div>
+  )
+}
+
+export default ManageSubmission
---
+++ src/components/participant-components/ManageTeam.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManageTeam = () => {
+  return (
+    <div>
+      You can manage your team here
+    </div>
+  )
+}
+
+export default ManageTeam
